1fe0fe460a52e46a4e3849d48b3953fe0bfb07de
wilembergson <wilembergson@yahoo.com>
2020-05-30 17:31:37 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,7 +24,11 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		
+		System.out.println("\n=== TEST 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
 	}
 
 }
